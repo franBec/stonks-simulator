5d4ab0676edffda97517571fb5b3e0e533101104
FranBec <franbecvort@gmail.com>
2026-05-02 17:57:18 +0100

    Add Spring Boot backend project

---
+++ stonks_java/src/main/java/dev/pollito/stonks_java/StonksJavaApplication.java
@@ -0,0 +1,13 @@
+package dev.pollito.stonks_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StonksJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StonksJavaApplication.class, args);
+	}
+
+}
---
+++ stonks_java/src/test/java/dev/pollito/stonks_java/StonksJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pollito.stonks_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StonksJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
